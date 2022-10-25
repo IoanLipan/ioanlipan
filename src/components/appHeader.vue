91fcb9c45641efcd1365225daabd352987302ae2
Lipan Ioan <lipanioan420@gmail.com>
2022-10-25 09:50:24 +0300

    this is the first commit

---
+++ src/components/appHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <!-- desktop menu -->
+    <div class="bg-blue-700 p-4 grid grid-cols-12 text-white">
+      <div class="col-start-2 col-span-10 flex justify-between items-center">
+        <!-- menu -->
+        <div class="text-3xl">Ioan Lipan</div>
+        <div></div>
+        <div>Web Developer</div>
+        <div>Engineer</div>
+        <div>Athlete</div>
+        <div>Problem-Solver</div>
+      </div>
+    </div>
+    <!-- mobile menu -->
+    <div class="md:hidden">asdad</div>
+  </nav>
+</template>
