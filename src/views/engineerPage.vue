7c63e31927c3f12306fa37aad166821f5e747cfb
Lipan Ioan <lipanioan420@gmail.com>
2022-10-26 19:21:41 +0300

    engineed page almost ready

--- src/views/engineerPage.vue
+++ src/views/engineerPage.vue
@@ -1,8 +1,60 @@
 <template>
-  <div class="grid-of-12-container bg-green-200 mx-auto h-full w-full">
-    <div class="cols-2-10">
-        <div class="">sdfs</div>
-      <!-- <div class="text-5xl">this is the engineer page</div> -->
+  <div class="grid-of-12-container mt-auto">
+    <div class="col-start-2 col-span-8">
+      <ol class="relative border-l border-gray-200">
+        <li class="ml-10 mb-10">
+          <span class="flex absolute -left-5 justify-center items-center w-10 h-10
+             bg-blue-200 rounded-full ring-8 ring-white">
+             <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M15.517 24h-11.646c.522-3.035.897-6.162-.422-8.028-1.666-2.357-2.43-4.742-2.449-6.883-.045-5.19 4.231-9.114 10.203-9.089 7.236.03 9.328 6.156 9.773 7.943.34 1.369-.898 1.869-.166 2.702.596.679 1.035 1.364 1.789 2.177.292.315.405.646.401.943-.006.434-.291.798-.748.958-.429.15-.76.32-1.215.443-.145 1.16-.521 2.572-.798 3.557-.737 2.62-2.896 1.059-3.881 2.607-.426.668-.64 1.738-.841 2.67zm-3.844-19h-1.346c-.243.681-.312 1.122-.842 1.341-.53.22-.888-.041-1.545-.353l-.952.952c.311.654.573 1.015.353 1.545-.219.53-.66.599-1.341.841v1.347c.68.242 1.122.312 1.341.842.222.534-.047.902-.353 1.544l.952.952c.652-.309 1.015-.573 1.545-.353v.001c.529.219.599.657.842 1.341h1.346c.243-.682.313-1.121.845-1.343h.001c.526-.219.883.042 1.541.354l.952-.952c-.31-.651-.573-1.014-.354-1.544.219-.529.662-.6 1.342-.842v-1.347c-.688-.244-1.123-.313-1.341-.841-.22-.53.041-.89.353-1.545l-.952-.952c-.651.31-1.014.573-1.545.353-.529-.219-.598-.657-.842-1.341zm-.673 6.667c-.92 0-1.667-.747-1.667-1.667 0-.921.747-1.667 1.667-1.667s1.667.746 1.667 1.667c0 .92-.747 1.667-1.667 1.667z"/></svg></span>
+          <h3 class="mb-1 text-lg font-semibold text-gray-900">
+            Engineer Diploma
+            <span class="bg-blue-100 text-blue-800 text-sm font-medium
+                mr-2 px-2.5 py-0.5 rounded ml-3">
+              Latest
+            </span>
+          </h3>
+          <time class="block mb-2 text-sm font-normal leading-none text-gray-400">
+            2022
+          </time>
+          <p class="text-base font-normal text-gray-500">
+            I finished the Faculty of Electronics, Telecommunications and Technology Information.
+            My lincense project was a Traffic Sign Detection Application that used a CNN
+            (Convolutional Neural Network) to train a model to detect traffic signs.
+          </p>
+        </li>
+        <li class="ml-10 mb-10">
+          <span class="flex absolute -left-5 justify-center items-center w-10 h-10
+             bg-blue-200 rounded-full ring-8 ring-white">
+             <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve"> <g> <path d="M49.367,20.627L26.713,9.301c-0.838-0.421-2.223-0.421-3.062,0L0.998,20.627c-0.845,0.419-0.845,1.107,0,1.535l1.269,0.635 v4.715c-0.43,0.117-0.754,0.489-0.754,0.954c0,0.366,0.205,0.679,0.502,0.853l-1.94,7.183C0.03,36.594,0,36.688,0,36.784l0,0v0.002 l0,0c0,0.662,1.129,1.2,2.521,1.2c1.389,0,2.517-0.538,2.521-1.2l0,0v-0.002l0,0c0-0.096-0.033-0.188-0.078-0.283l-1.937-7.183 c0.294-0.174,0.501-0.486,0.501-0.853c0-0.465-0.325-0.838-0.753-0.954v-4.465l9.054,4.532c-0.063,0.161-0.108,0.33-0.108,0.503 v9.495c0,0.002-0.001,0.011-0.001,0.014c0,1.888,6.028,3.425,13.458,3.425c7.437,0,13.462-1.537,13.462-3.425 c0-0.011-0.004-0.016-0.004-0.021c0,0.004,0.008,0.007,0.008,0.018v-9.506c0-0.173-0.045-0.342-0.11-0.503l10.834-5.417 C50.211,21.734,50.211,21.046,49.367,20.627z M25.177,40.159c-8.184,0-12.601-1.826-12.601-2.567c0-0.773,4.291-2.578,12.601-2.578 c8.318,0,12.603,1.805,12.603,2.578C37.779,38.332,33.368,40.159,25.177,40.159z"/> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> </svg></span>
+          <h3 class="mb-1 text-lg font-semibold text-gray-900">
+            Polytehnic University of Bucharest
+          </h3>
+          <time class="block mb-2 text-sm font-normal leading-none text-gray-400">
+            2018 - 2022
+          </time>
+          <p class="text-base font-normal text-gray-500">
+            I graduated the Faculty of Electronics, Telecommunications and Technology Information,
+            and during the 4 years I encountered a lot of programming subjects: OOP, MatLab, Python
+            This helped me better understand the concepts and I found my passion.
+          </p>
+        </li>
+        <li class="ml-10">
+          <span class="flex absolute -left-5 justify-center items-center w-10 h-10
+             bg-blue-200 rounded-full ring-8 ring-white">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M23 5v13.883l-1 .117v-16c-3.895.119-7.505.762-10.002 2.316-2.496-1.554-6.102-2.197-9.998-2.316v16l-1-.117v-13.883h-1v15h9.057c1.479 0 1.641 1 2.941 1 1.304 0 1.461-1 2.942-1h9.06v-15h-1zm-12 13.645c-1.946-.772-4.137-1.269-7-1.484v-12.051c2.352.197 4.996.675 7 1.922v11.613zm9-1.484c-2.863.215-5.054.712-7 1.484v-11.613c2.004-1.247 4.648-1.725 7-1.922v12.051z"/></svg>
+          </span>
+          <h3 class="mb-1 text-lg font-semibold text-gray-900">
+            National High-School "Ferdinand I", Bacau
+          </h3>
+          <time class="block mb-2 text-sm font-normal leading-none text-gray-400">
+            2014 - 2018
+          </time>
+          <p class="text-base font-normal text-gray-500">
+            I finished the mathematical-informatical profile,
+            scoring 10/10 in the mathematics Bacalaureate.
+          </p>
+        </li>
+      </ol>
     </div>
   </div>
 </template>
