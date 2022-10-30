d45dd9eb36f78495a30a0f81eefde536740bb661
Lipan Ioan <lipanioan420@gmail.com>
2022-10-30 23:38:14 +0200

    fix colors and padding web dev page

--- src/views/webDeveloperPage.vue
+++ src/views/webDeveloperPage.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="grid-of-12-container">
-    <div class="text-white text-2xl text-center
+    <div class="text-white text-2xl text-center font-serif tracking-widest leading-relaxed
       col-start-2 col-span-10 md:col-start-4 md:col-span-6 pt-10">
       Programming is my passion. My mindstate is that anything can be
-      <span class="hover:text-blue-400 cursor-pointer">created</span>,
-      <span class="hover:text-green-400 cursor-pointer">improved</span> and
-      <span class="hover:text-red-500 cursor-pointer">fixed</span> if
-      <span class="hover:text-yellow-400 cursor-pointer">broken</span>. Here are some of the
-      projects I have been <span class="hover:text-blue-600 cursor-pointer">blessed</span> to
-      <span class="font-bold">work on</span> and
-      <span class="text-bold underline">learn from</span>:
+      <span class="text-blue-400">created</span>,
+      <span class="text-green-400">improved</span> and
+      <span class="text-red-500">fixed</span> if
+      <span class="text-yellow-400">broken</span>. Here are some of the
+      projects I have been <span class="text-blue-600">blessed</span> to
+      <span class="font-superstar text-green-600">work on and learn from</span>
     </div>
     <div class="cols-2-10">
-      <div class="text-normal p-10 flex flex-row flex-wrap justify-evenly">
+      <div class="text-normal p-5 sm:p-10 flex flex-row flex-wrap justify-evenly">
         <ProjectCard
           title="UI library"
           usedTech="VueJS"
@@ -20,7 +19,7 @@
           imageName="library"
         />
         <ProjectCard
-          title="TheLoupesCompany"
+          title="TLC"
           usedTech="ReactJS, TailwindCss"
           description="A e-commerce website for a dentist accessories company"
           imageName="e-commerce"
