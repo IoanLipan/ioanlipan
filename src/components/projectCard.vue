bc8616308219c5624efb05607bf7bca8db7af903
Lipan Ioan <lipanioan420@gmail.com>
2022-10-30 23:00:18 +0200

    fix cards

--- src/components/projectCard.vue
+++ src/components/projectCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-white font-mono flex flex-col text-center gap-4
-    p-10 md:w-[40%] bg-slate-600 border-slate-600 rounded-xl mb-12" >
+    p-10 w-[90%] lg:w-[40%] bg-slate-600 border-slate-600 rounded-xl mb-12" >
     <SvgIcon :name="imageName" class="self-center fill-white" />
     <h3 class="text-2xl text-lime-300 underline">{{ title }}</h3>
     <h4 class="text-orange-400 w-fit mx-auto px-5 py-2 border-slate-600 rounded-xl bg-slate-500">
--- src/views/webDeveloperPage.vue
+++ src/views/webDeveloperPage.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="grid-of-12-container">
+    <div class="text-white text-2xl text-center
+      col-start-2 col-span-10 md:col-start-4 md:col-span-6 pt-10">
+      Programming is my passion. My mindstate is that anything can be
+      <span class="hover:text-blue-400 cursor-pointer">created</span>,
+      <span class="hover:text-green-400 cursor-pointer">improved</span> and
+      <span class="hover:text-red-500 cursor-pointer">fixed</span> if
+      <span class="hover:text-yellow-400 cursor-pointer">broken</span>. Here are some of the
+      projects I have been <span class="hover:text-blue-600 cursor-pointer">blessed</span> to
+      <span class="font-bold">work on</span> and
+      <span class="text-bold underline">learn from</span>:
+    </div>
     <div class="cols-2-10">
       <div class="text-normal p-10 flex flex-row flex-wrap justify-evenly">
         <ProjectCard
