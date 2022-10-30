97fe8616b7c2602c058c3f58633765fa3a6760a7
Lipan Ioan <lipanioan420@gmail.com>
2022-10-30 17:02:05 +0200

    fix header fixed position

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -1,28 +1,37 @@
 <template>
   <nav class="h-[72px] md:h-20">
     <div class="w-full fixed bg-black p-5 text-white">
-      <div class="grid grid-cols-3 items-center">
+      <div class="grid grid-cols-4 items-center">
         <div class="space-y-2 w-8 ease-in transition-all duration-500" @click="toggleMenu">
-          <div :class="['h-0.5 bg-gray-200 transition-all duration-400',
-            showMenu ? 'rotate-45 translate-y-[5px]' : '']" />
-          <div :class="['h-0.5 bg-gray-200 transition-all duration-300',
-            showMenu ? 'hidden' : '']" />
-          <div :class="['h-0.5 bg-gray-200 transition-all duration-400',
-            showMenu ? '-rotate-45 -translate-y-[5px]' : '']" />
+          <div
+            :class="[
+              'h-0.5 bg-gray-200 transition-all duration-400',
+              showMenu ? 'rotate-45 translate-y-[5px]' : '',
+            ]"
+          />
+          <div
+            :class="['h-0.5 bg-gray-200 transition-all duration-300', showMenu ? 'hidden' : '']"
+          />
+          <div
+            :class="[
+              'h-0.5 bg-gray-200 transition-all duration-400',
+              showMenu ? '-rotate-45 -translate-y-[5px]' : '',
+            ]"
+          />
         </div>
-        <h1 class="font-cinzel text-2xl md:text-4xl col-span-2 md:col-span-1 text-center">
-          Ioan Lipan
-        </h1>
+        <h1 class="font-cinzel text-2xl md:text-4xl col-span-2 text-center">Ioan Lipan</h1>
       </div>
-      <div v-show="showMenu" class="h-[92vh] z-50 md:max-w-[40vw]">
-        <div class="h-full flex flex-col gap-10 text-3xl pl-5">
-          <div></div>
-          <HeaderMenuItem title="Home" link="/" @click="closeMenu" />
-          <HeaderMenuItem title="Web-Developer" link="/web-dev" @click="closeMenu" />
-          <HeaderMenuItem title="Engineer" link="/engineer" @click="closeMenu" />
-          <HeaderMenuItem title="Athlete" link="/athlete" @click="closeMenu" />
-          <HeaderMenuItem title="Problem-Solver" link="/problem-solver" @click="closeMenu" />
-        </div>
+    </div>
+    <div
+      v-show="showMenu"
+      class="h-[92vh] top-[72px] fixed z-50 bg-black md:bg-opacity-90
+      text-white w-full md:w-[36vw] xl:w-[20vw] px-6" >
+      <div class="h-full flex flex-col justify-evenly text-3xl pl-5 text-center md:text-left">
+        <HeaderMenuItem title="Home" link="/" @click="closeMenu" />
+        <HeaderMenuItem title="Web-Developer" link="/web-dev" @click="closeMenu" />
+        <HeaderMenuItem title="Engineer" link="/engineer" @click="closeMenu" />
+        <HeaderMenuItem title="Athlete" link="/athlete" @click="closeMenu" />
+        <HeaderMenuItem title="Problem-Solver" link="/problem-solver" @click="closeMenu" />
       </div>
     </div>
   </nav>
