56fe3626ad038d011a89102c5738306a48f9a13f
Lipan Ioan <lipanioan420@gmail.com>
2022-11-06 11:06:30 +0200

    fixed images

---
+++ src/components/normalImage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- <img v-if="name === basketball" :src="'@/assets/' + basketball" /> -->
+    <img v-if="imageName === basketball" :src="'../assets/images/' + this.basketball" />
+    <img v-else-if="imageName === kickbox" :src="'../assets/images/' + this.kickbox" />
+    <img v-else-if="imageName === gym" :src="'../assets/images/' + this.gym" />
+    <img v-else-if="imageName === pingpong" :src="'../assets/images/' + this.pingpong" />
+    <img v-else :src="'../assets/' + this.placeholder" />
+  </div>
+</template>
+<script>
+export default {
+  name: 'ImageComponent',
+  props: {
+    imageName: {
+      type: String,
+      default: 'placeholder',
+    },
+  },
+  data() {
+    return {
+      basketball: 'basketball.jpg',
+      kickbox: 'kickbox.jpeg',
+      gym: 'gym.jpeg',
+      pingpong: 'pingpong.jpeg',
+      placeholder: 'placeholder.png',
+    };
+  },
+};
+</script>
---
+++ src/components/timeLineItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <li class="ml-10 mb-10">
+    <span
+      class="flex absolute -left-5 justify-center items-center w-10 h-10
+      bg-blue-200 rounded-full ring-8 ring-white"
+    >
+      <svgIcon :name="svgName" />
+    </span>
+    <h3 class="mb-1 text-lg font-semibold text-white">
+      {{ title }}
+      <span v-if="svgName === 'diploma'"
+      class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded ml-3">
+        Latest
+      </span>
+    </h3>
+    <time class="block mb-2 text-sm font-normal leading-none text-gray-400"> {{ period }} </time>
+    <p class="text-base font-normal text-white">
+      {{ description }}
+    </p>
+  </li>
+</template>
+
+<script>
+import svgIcon from './svgIcon.vue';
+
+export default {
+  name: 'TimeLineItem',
+  components: { svgIcon },
+  props: {
+    title: {
+      type: String,
+      default: 'timeline item',
+    },
+    period: {
+      type: String,
+      default: '2221-2222',
+    },
+    svgName: {
+      type: String,
+      default: 'diploma',
+    },
+    description: {
+      type: String,
+      default: 'lorem',
+    },
+  },
+};
+</script>
