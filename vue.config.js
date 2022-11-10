1a5e98e3056554ad67c299208dc23c57dd12929b
Lipan Ioan <lipanioan420@gmail.com>
2022-11-10 15:14:40 +0200

    remove settings from vue.cfg

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,4 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  settings: {
-    'import/resolver': {
-      webpack: {
-        config: require.resolve('@vue/cli-service/webpack.config.js'),
-      },
-    },
-  },
 });
