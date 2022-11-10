4b658e62ada7e119ff69e8bb383e0ce5a5605f1c
Lipan Ioan <lipanioan420@gmail.com>
2022-11-10 15:11:12 +0200

    header menu about me

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="h-[72px] md:h-20 z-40">
-    <div class="w-full fixed bg-black p-5 text-white">
+    <div class="w-full fixed bg-black p-5 text-white font-mono">
       <div class="grid grid-cols-4 items-center">
         <div class="space-y-2 w-8 ease-in transition-all duration-500" @click="toggleMenu">
           <div
@@ -19,22 +19,31 @@
             ]"
           />
         </div>
-        <router-link class="font-mono text-2xl md:text-4xl col-span-2 text-center" to="/"
-          @click="closeMenu()">Ioan Lipan</router-link
+        <router-link
+          class="font-mono text-2xl md:text-4xl col-span-2 text-center"
+          to="/"
+          @click="closeMenu()"
+          >Ioan Lipan</router-link
         >
       </div>
     </div>
     <div
       v-show="showMenu"
       class="h-[92vh] top-[72px] fixed z-50 bg-black md:bg-opacity-90
-      text-white w-full md:w-[36vw] xl:w-[25vw] px-6" v-motion-slide-visible-left
+      text-white w-full md:w-[36vw] xl:w-[25vw] px-6"
     >
       <div class="h-full flex flex-col gap-8 text-3xl pt-10 text-center md:text-left">
         <div></div>
         <HeaderMenuItem title="🧠 Web-Developer" link="/" @click="closeMenu" />
-        <HeaderMenuItem title="🔧 Engineer" link="/engineer" @click="closeMenu" />
-        <HeaderMenuItem title="⛹🏼‍♂️ Athlete" link="/athlete" @click="closeMenu" />
-        <HeaderMenuItem title="🧐 Problem-Solver" link="/problem-solver" @click="closeMenu" />
+        <div v-if="!showAboutMe" @click="toggleAboutMe" class="p-2">🦁 About me ↘️</div>
+        <div v-else class="flex flex-col gap-8 p-2" @click="toggleAboutMe">
+          🦁 About me ↗️
+          <HeaderMenuItem title="🔧 Engineer" link="/engineer" @click="closeMenu" v-motion-pop />
+          <HeaderMenuItem title="⛹🏼‍♂️ Athlete" link="/athlete" @click="closeMenu" v-motion-pop />
+          <HeaderMenuItem title="🧐 Problem-Solver"
+            link="/problem-solver"
+            @click="closeMenu" v-motion-pop />
+        </div>
       </div>
     </div>
   </nav>
@@ -50,9 +59,13 @@ export default {
     return {
       showDropdown: false,
       showMenu: false,
+      showAboutMe: false,
     };
   },
   methods: {
+    toggleAboutMe() {
+      this.showAboutMe = !this.showAboutMe;
+    },
     toggleMenu() {
       this.showMenu = !this.showMenu;
     },
