aa6e652b33e66e5ab4d7a716604062597102fd65
Lipan Ioan <lipanioan420@gmail.com>
2022-11-10 14:58:51 +0200

    problem solver page

---
+++ src/components/problemCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="text-center bg-slate-600 border-slate-800 border-2 rounded-2xl
+   max-w-fit mx-auto flex flex-col justify-between cursor-grab
+   h-[380px] sm:h-[480px] p-8">
+    <img class="h-48 sm:h-80 w-auto rounded-xl mx-auto" :src="imagePath" />
+    <div>
+      <div class="text-2xl pb-2">{{ title }}</div>
+      <div class="max-w-lg mb2">{{ description }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProblemCard',
+  props: {
+    title: {
+      type: String,
+      default: 'blablabla',
+    },
+    description: {
+      type: String,
+      default: 'blablabla',
+    },
+    image: {
+      type: String,
+      default: 'placeholder.png',
+    },
+  },
+  computed: {
+    imagePath() {
+      if (this.image !== '') {
+        return require(`@/assets/images/${this.image}`);
+      }
+      return 0;
+    },
+  },
+};
+</script>
--- src/views/problemSolverPage.vue
+++ src/views/problemSolverPage.vue
@@ -1,24 +1,71 @@
 <template>
   <div class="grid-of-12-container">
-    <div class="cols-2-10 mt-10 font-mono text-white">
-      <h1 class="text-3xl font-semibold">I'm always looking for challanges!</h1>
-      <p class="text-lg">
-        Anything that implies being smart and adaptable</p>
-      <div class="text-normal">
-        <div>rubiks speed cubing</div>
-        <div>puzzle</div>
-        <div>other rubiks</div>
-        <div>math</div>
-        <div>lego</div>
-        <div>chess</div>
-      </div>
-      <div class="text-5xl">this is the problem solving page</div>
+    <div class="cols-2-10 mt-10 font-mono text-white text-center">
+      <h1 class="text-3xl font-semibold">I'm always looking a new challenge!</h1>
+      <p class="text-lg">Anything that implies being smart and adaptable</p>
+      <swiper
+        :modules="modules"
+        :space-between="20"
+        :loop="true"
+        :pagination="{ clickable: true }"
+        :autoplay="{
+          delay: 5000,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,
+        }"
+        class="my-10"
+      >
+        <swiper-slide v-for="problem in swiperProblems" :key="problem.description">
+          <ProblemCard
+            :image="problem.image"
+            :title="problem.title"
+            :description="problem.description"
+          />
+        </swiper-slide>
+      </swiper>
     </div>
   </div>
 </template>
 
 <script>
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
+import { Autoplay, Pagination } from 'swiper';
+import { ref } from 'vue';
+import ProblemCard from '../components/problemCard.vue';
+import 'swiper/swiper-bundle.min.css';
+
 export default {
   name: 'ProblemSolverPage',
+  components: { Swiper, SwiperSlide, ProblemCard },
+  setup() {
+    const swiperProblems = ref([
+      {
+        image: 'rubiks.jpeg',
+        title: 'Rubiks Cube',
+        description: "I can solve a rubik's cube in less than a minute",
+      },
+      {
+        image: 'chess.jpeg',
+        title: 'Chess',
+        description: 'I think chess is the best strategy game you can play',
+      },
+      {
+        image: 'jigsawPuzzle.jpeg',
+        title: 'Jigsaw Puzzle',
+        description: 'These kind of puzzles train your patience. Trust me.',
+      },
+      {
+        image: 'otherRubiks.jpeg',
+        title: 'Other Rubiks Puzzles',
+        description: 'I tried almost evey variation of rubik puzzle',
+      },
+      {
+        image: 'mathLogic.jpeg',
+        title: 'Math and logic problems',
+        description: 'I love math and logic',
+      },
+    ]);
+    return { modules: [Pagination, Autoplay], swiperProblems };
+  },
 };
 </script>
