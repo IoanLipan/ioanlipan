79cb2c24129821dda2c9dcb42d2f442c7791377d
Lipan Ioan <lipanioan420@gmail.com>
2022-11-12 15:57:07 +0200

    different animation for first athlete article

--- src/components/textWithImage.vue
+++ src/components/textWithImage.vue
@@ -1,14 +1,45 @@
 <template>
+  <!-- this is for the next articles -->
   <div class="blog-container-grid-2-1">
-    <img v-if="!imageIsRight" class="blog-image-desktop" :src="image" v-motion-pop-visible />
-    <div>
-      <h1 class="blog-title" v-motion-slide-left>{{ title }}</h1>
+    <img
+      v-if="!imageIsRight && title !== 'Basketball'"
+      class="blog-image-desktop"
+      :src="image"
+      v-motion-pop-visible
+    />
+    <div v-if="title !== 'Basketball'">
+      <h1 class="blog-title" v-motion-slide-visible-left>{{ title }}</h1>
       <img :src="image" class="blog-image-mobile" v-motion-pop-visible />
       <p class="blog-text" v-motion-fade-visible>
         {{ text }}
       </p>
     </div>
-    <img v-if="imageIsRight" class="blog-image-desktop" :src="image" v-motion-pop-visible />
+    <img
+      v-if="imageIsRight && title !== 'Basketball'"
+      class="blog-image-desktop"
+      :src="image"
+      v-motion-pop-visible
+    />
+  <!-- this is for the first article -->
+    <img
+      v-if="!imageIsRight && title === 'Basketball'"
+      class="blog-image-desktop"
+      :src="image"
+      v-motion-pop
+    />
+    <div v-if="title === 'Basketball'">
+      <h1 class="blog-title" v-motion-slide-left>{{ title }}</h1>
+      <img :src="image" class="blog-image-mobile" v-motion-pop />
+      <p class="blog-text" v-motion-fade>
+        {{ text }}
+      </p>
+    </div>
+    <img
+      v-if="imageIsRight && title === 'Basketball'"
+      class="blog-image-desktop"
+      :src="image"
+      v-motion-pop
+    />
   </div>
 </template>
 
