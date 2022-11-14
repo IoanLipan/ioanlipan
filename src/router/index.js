a0bd862a491ddb4d5d156cd26e84a3e8c0be96ef
Lipan Ioan <lipanioan420@gmail.com>
2022-11-14 23:57:03 +0200

    404, deleted unused fonts

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import WebDev from '../views/webDeveloperPage.vue';
-import Athlete from '../views/athletePage.vue';
-import Engineer from '../views/engineerPage.vue';
-import ProblemSolver from '../views/problemSolverPage.vue';
+// import Athlete from '../views/athletePage.vue';
+// import Engineer from '../views/engineerPage.vue';
+// import ProblemSolver from '../views/problemSolverPage.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'WebDev',
+    name: 'home',
     component: WebDev,
+    meta: { title: 'Ioan Lipan' },
     // this is for lazyloading
-    // component: () => import('../views/webDeveloper.vue'),
+    // component: () => import('../views/webDeveloperPage.vue'),
   },
   {
     path: '/athlete',
-    name: 'Athlete',
-    component: Athlete,
+    name: 'athlete',
+    // component: Athlete,
+    component: () => import('../views/athletePage.vue'),
+    meta: { title: 'Athlete' },
   },
   {
     path: '/engineer',
-    name: 'Engineer',
-    component: Engineer,
+    name: 'engineer',
+    // component: Engineer,
+    component: () => import('../views/engineerPage.vue'),
+    meta: { title: 'Engineer' },
   },
   {
     path: '/problem-solver',
-    name: 'ProblemSolver',
-    component: ProblemSolver,
+    name: 'problem-solver',
+    // component: ProblemSolver,
+    component: () => import('../views/problemSolverPage.vue'),
+    meta: { title: 'Problem Solver' },
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'not-found',
+    // component: ProblemSolver,
+    component: () => import('../views/pageNotFound.vue'),
+    meta: { title: 'Not Found' },
   },
 ];
 
---
+++ src/views/pageNotFound.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="grid-of-12-container py-16 md:py-20 bg-slate-800 font-mono">
+    <div class="col-start-3 col-span-8 text-center">
+      <div class="text-white text-2xl md:text-4xl mb-16">
+        Sorry, this page doesn't exist! 😔
+      </div>
+      <router-link
+        class="p-5 bg-slate-500 hover:bg-slate-600 text-white text-2xl rounded-xl"
+        :to="{ name: 'home' }"
+      >
+        Back To Home
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Not-found',
+};
+</script>
