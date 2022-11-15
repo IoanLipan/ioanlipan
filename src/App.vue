2541366776f995d19785002279667e131011c863
Lipan Ioan <lipanioan420@gmail.com>
2022-11-15 15:58:17 +0200

    arrow top, svg's

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@
     <AppHeader />
     <router-view />
     <ContactMe v-motion-pop />
+    <ScrollTopArrow />
   </div>
 </template>
 
 <script>
 import AppHeader from './components/appHeader.vue';
 import ContactMe from './components/footerMail.vue';
+import ScrollTopArrow from './components/scrollTopArrow.vue';
 
 export default {
   name: 'App',
   components: {
     AppHeader,
     ContactMe,
+    ScrollTopArrow,
   },
 };
 </script>
---
+++ src/components/scrollTopArrow.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    @click="scrollTop"
+    v-show="visibleScroll"
+    class="fill-slate-400 fixed bottom-3 right-3 md:bottom-5 md:right-5 cursor-pointer z-30"
+  >
+    <SvgIcon name="arrowUp" class="w-24 md:w-36" />
+  </div>
+</template>
+
+<script>
+import SvgIcon from './svgIcon.vue';
+
+export default {
+  name: 'ScrollTopArrow',
+  components: { SvgIcon },
+  data() {
+    return {
+      visibleScroll: false,
+    };
+  },
+  methods: {
+    scrollTop() {
+      this.intervalId = setInterval(() => {
+        if (window.pageYOffset === 0) {
+          clearInterval(this.intervalId);
+        }
+        window.scroll(0, window.pageYOffset - 50);
+      }, 20);
+    },
+    scrollListener() {
+      this.visibleScroll = window.scrollY > 100;
+    },
+  },
+  mounted() {
+    window.addEventListener('scroll', this.scrollListener);
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.scrollListener);
+  },
+};
+</script>
