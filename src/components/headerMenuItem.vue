998e740fd9f451251cd4df0f7e96dc4926b4e383
Lipan Ioan <lipanioan420@gmail.com>
2023-05-07 17:30:04 +0300

    add menu item hover effect

--- src/components/headerMenuItem.vue
+++ src/components/headerMenuItem.vue
@@ -1,7 +1,8 @@
 <template>
   <router-link
     :to="{ path: link }"
-    class="p-2 font-mono flex items-center"
+    class="p-2 font-mono flex items-center
+      lg:hover:bg-slate-700 lg:not:hover:bg-transparent rounded-lg"
     :class="iconName ? 'flex-row gap-3' : 'flex-col'"
   >
     <SvgIcon v-if="iconName" :name="iconName" />
