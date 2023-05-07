3d6a400d908a4b8cbb15b9781d3c60e8b1f1f178
Lipan Ioan <lipanioan420@gmail.com>
2023-05-07 14:49:37 +0300

    add tech page

---
+++ src/components/knownTechItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class='flex flex-col hover:bg-slate-700 p-5 rounded-full'>
+    <div class="w-24 h-24 flex items-center justify-center">
+      <SvgIcon :name='imageName' />
+    </div>
+    <!-- <h2>{{ title }}</h2>
+    <p>{{ description }}</p>
+    <p>{{ level }}</p> -->
+  </div>
+</template>
+
+<script>
+import SvgIcon from './svgIcon.vue';
+
+export default {
+  name: 'KnownTechItem',
+  components: { SvgIcon },
+  props: {
+    title: {
+      type: String,
+      default: 'Tech',
+    },
+    imageName: {
+      type: String,
+      default: 'ILLogoSvg',
+    },
+    description: {
+      type: String,
+      default: 'Lorem ipsum igitus sqgwefvf sdcv r fverc vetgrwce',
+    },
+    level: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
---
+++ src/views/knownTechs.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="text-2xl md:text-4xl text-white font-mono grid grid-cols-12">
+    <div class="col-start-2 col-span-10 md:col-start-3 md:col-span-8 text-center p-4 md:p-10">
+      These are the technologies that I am proficient in:
+    </div>
+    <div class="col-start-2 col-span-10 flex justify-evenly items-center flex-wrap">
+          <KnownTechItem
+            imageName="html"
+            title="asa"
+            description="asdfasdfasdfasdf"
+            level="leevl experience"
+          />
+          <KnownTechItem
+          imageName="css"
+          title="asa"
+          description="asdfasdfasdfasdf"
+          level="leevl experience"
+        />
+        <KnownTechItem
+          imageName="tailwind"
+          title="asa"
+          description="asdfasdfasdfasdf"
+          level="leevl experience"
+        />
+        <KnownTechItem
+        imageName="javascript"
+        title="asa"
+        description="asdfasdfasdfasdf"
+        level="leevl experience"
+        />
+        <KnownTechItem
+        imageName="react"
+        title="asa"
+        description="asdfasdfasdfasdf"
+        level="leevl experience"
+        />
+        <KnownTechItem
+        imageName="vue"
+        title="asa"
+        description="asdfasdfasdfasdf"
+        level="leevl experience"
+        />
+    </div>
+  </div>
+</template>
+
+<script>
+import KnownTechItem from '../components/knownTechItem.vue';
+
+export default {
+  name: 'KnownTechs',
+  title: 'Ioan Lipan | Tech-Knowledge',
+  components: { KnownTechItem },
+};
+</script>
