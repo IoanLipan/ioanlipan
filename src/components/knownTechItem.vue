64f876c026282b4c758f5d98f2fe3d510a15a4f4
Lipan Ioan <lipanioan420@gmail.com>
2023-07-04 19:02:03 +0300

    add modal for techs

--- src/components/knownTechItem.vue
+++ src/components/knownTechItem.vue
@@ -1,20 +1,28 @@
 <template>
-  <div class='flex flex-col hover:bg-slate-700 p-5 rounded-full'>
-    <div class="w-24 h-24 flex items-center justify-center">
-      <SvgIcon :name='imageName' />
+  <div>
+    <div class='flex flex-col hover:bg-slate-700 transition-all p-5 rounded-full' @click="openModal">
+      <div class="w-24 h-24 flex items-center justify-center">
+        <SvgIcon :name='imageName' />
+      </div>
     </div>
-    <!-- <h2>{{ title }}</h2>
-    <p>{{ description }}</p>
-    <p>{{ level }}</p> -->
+    <div class="modal-overlay" v-if="isModalOpen" @click="closeModal">
+      <div class="modal text-center flex flex-col justify-around gap-8 rounded-[36px] font-mono">
+        <SvgIcon :name='imageName' class="mx-auto" />
+        <h2 class="text-white">{{ title }}</h2>
+        <StarRating :level="level" />
+        <p class="text-slate-400 text-base">{{ description }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import SvgIcon from './svgIcon.vue';
+import StarRating from './starRating.vue';
 
 export default {
   name: 'KnownTechItem',
-  components: { SvgIcon },
+  components: { SvgIcon, StarRating },
   props: {
     title: {
       type: String,
@@ -29,9 +37,59 @@ export default {
       default: 'Lorem ipsum igitus sqgwefvf sdcv r fverc vetgrwce',
     },
     level: {
-      type: String,
-      default: '',
+      type: Number,
+      default: 1,
+    },
+  },
+  data() {
+    return {
+      isModalOpen: false,
+    };
+  },
+  methods: {
+    openModal() {
+      this.isModalOpen = true;
+    },
+    closeModal() {
+      this.isModalOpen = false;
     },
   },
 };
 </script>
+
+<style>
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(34, 35, 34, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal {
+  background-color: rgb(75, 73, 73);
+  padding: 20px;
+  max-width: 400px;
+  border-radius: 36px;
+  text-align: center;
+  animation-name: modal-animation;
+  animation-duration: 0.15s;
+  animation-timing-function: ease-in-out;
+}
+
+@keyframes modal-animation {
+  from {
+    opacity: 0;
+    transform: scale(0.6);
+  }
+
+  to {
+    opacity: 1;
+    transform: scale(1);
+  }
+}
+</style>
--- src/views/knownTechs.vue
+++ src/views/knownTechs.vue
@@ -4,12 +4,26 @@
       These are the technologies that I am proficient in:
     </div>
     <div class="col-start-2 col-span-10 flex justify-evenly items-center flex-wrap">
-      <KnownTechItem imageName="html" title="asa" description="asdfasdfasdfasdf" level="leevl experience" />
-      <KnownTechItem imageName="css" title="asa" description="asdfasdfasdfasdf" level="leevl experience" />
-      <KnownTechItem imageName="tailwind" title="asa" description="asdfasdfasdfasdf" level="leevl experience" />
-      <KnownTechItem imageName="javascript" title="asa" description="asdfasdfasdfasdf" level="leevl experience" />
-      <KnownTechItem imageName="react" title="asa" description="asdfasdfasdfasdf" level="leevl experience" />
-      <KnownTechItem imageName="vue" title="asa" description="asdfasdfasdfasdf" level="leevl experience" />
+      <KnownTechItem imageName="html" title="HTML"
+        description="HTML (Hypertext Markup Language) is the standard language used for creating the structure and content of web pages"
+        level="5" />
+      <KnownTechItem imageName="css" title="CSS"
+        description="CSS (Cascading Style Sheets) is a stylesheet language that controls the presentation and appearance of HTML elements on a web page."
+        level="5" />
+      <KnownTechItem imageName="tailwind" title="Tailwind"
+        description="Tailwind CSS is a utility-first CSS framework that provides a set of pre-defined classes to rapidly build user interfaces."
+        level="5" />
+      <KnownTechItem imageName="javascript" title="JavaScript" description="JavaScript is a versatile programming language used for adding interactivity and dynamic functionality to websites.
+
+" level="4" />
+      <KnownTechItem imageName="react" title="ReactJS" description="React is a popular JavaScript library for building user interfaces, offering a component-based approach for creating reusable UI elements.
+
+" level="3" />
+      <KnownTechItem imageName="vue" title="VueJS"
+        description="Vue.js is a progressive JavaScript framework for building user interfaces, known for its simplicity, flexibility, and performance."
+        level="4" />
+      <KnownTechItem imageName="shopify" title="Shopify"
+        description="Shopify is a popular e-commerce platform for creating and managing online stores." level="4" />
     </div>
   </div>
 </template>
