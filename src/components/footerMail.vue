a9299b61f5288232680930ad4e393446d98a6e7a
Lipan Ioan <lipanioan420@gmail.com>
2023-08-25 20:05:51 +0300

    max len message variable for mail

--- src/components/footerMail.vue
+++ src/components/footerMail.vue
@@ -2,77 +2,38 @@
 <template>
   <div class="grid-of-12-container w-[100vw]">
     <article
-      class="col-start-2 col-span-10 lg:col-start-3 lg:col-span-8 2xl:mx-20 p-4 md:p-6 bg-slate-900 border-slate-700 rounded-3xl my-10 font-mono text-slate-100"
-    >
-      <form
-        class="flex flex-col gap-4"
-        :action="FORM_ENDPOINT"
-        @submit="handleSubmit"
-        method="POST"
-        target="_parent"
-      >
+      class="col-start-2 col-span-10 lg:col-start-3 lg:col-span-8 2xl:mx-20 p-4 md:p-6 bg-slate-900 border-slate-700 rounded-3xl my-10 font-mono text-slate-100">
+      <form class="flex flex-col gap-4" :action="FORM_ENDPOINT" @submit="handleSubmit" method="POST" target="_parent">
         <h3 class="text-3xl py-6 text-center text-white">
           Contact me and lets start working on your website!
         </h3>
         <label class="text-xl text-red-500">Name:</label>
-        <input
-          name="name"
-          type="text"
-          ref="name"
-          required
-          @keyup="catchForbiddenWords()"
-          @paste="catchForbiddenWords()"
-          placeholder="Your Name ..."
-          maxlength="30"
-          class="p-3 rounded-2xl focus:outline-none focus:bg-red-200 text-black"
-        />
+        <input name="name" type="text" ref="name" required @keyup="catchForbiddenWords()" @paste="catchForbiddenWords()"
+          placeholder="Your Name ..." maxlength="30"
+          class="p-3 rounded-2xl focus:outline-none focus:bg-red-200 text-black w-full" />
         <label class="text-xl text-blue-400">Email:</label>
-        <input
-          name="email"
-          type="email"
-          ref="emailInput"
-          @blur="verifyEmail"
-          required
-          placeholder="Your Email ..."
-          maxlength="30"
-          class="p-3 rounded-2xl focus:outline-none focus:bg-blue-200 text-black"
-        />
+        <input name="email" type="email" ref="emailInput" @blur="verifyEmail" required placeholder="Your Email ..."
+          maxlength="30" class="p-3 rounded-2xl focus:outline-none focus:bg-blue-200 text-black w-full" />
         <label class="text-xl text-yellow-500">Your idea:</label>
-        <textarea
-          ref="messageInput"
-          name="message"
-          rows="5"
-          placeholder="Your extraordinary idea (max 250 characters) ..."
-          maxlength="250"
-          required
-          @keypress="getMessageInputLength()"
-          @keyup="getMessageInputLength(); catchForbiddenWords()"
+        <textarea ref="messageInput" name="message" rows="5"
+          placeholder="Your extraordinary idea (max 250 characters) ..." maxlength="250" required
+          @keypress="getMessageInputLength()" @keyup="getMessageInputLength(); catchForbiddenWords()"
           @paste="getMessageInputLength(); catchForbiddenWords();"
-          class="p-3 rounded-2xl focus:outline-none focus:bg-yellow-200 text-black"
-        />
-        <div :class="'relative text-end bottom-12 right-4 ' + getMessageColor">
-          maximum characters {{ messageInputLength }}/250
+          class="p-3 rounded-2xl focus:outline-none focus:bg-yellow-200 text-black w-full" />
+        <div :class="'relative text-end bottom-12 right-4 w-full ' + getMessageColor">
+          maximum characters {{ messageInputLength }}/{{ messageMaxLength }}
         </div>
         <div
           class="relative bottom-6 text-red-600 text-center text-lg bg-amber-400 w-3/4 md:w-2/3 p-3 rounded-3xl mx-auto"
-          v-if="!isEmailValid && isEmailValid !== 0"
-        >
+          v-if="!isEmailValid && isEmailValid !== 0">
           ⛔️ You should choose a valid email!
         </div>
         <div
           class="relative bottom-6 text-red-600 text-center text-lg bg-amber-400 w-3/4 md:w-2/3 p-3 rounded-3xl mx-auto"
-          v-if="forbiddenWords"
-        >
+          v-if="forbiddenWords">
           ⛔️ Let's speak like adults!
         </div>
-        <button
-          type="submit"
-          :class="
-            'text-2xl p-2 w-1/2 bg-slate-500 self-center ' +
-            submitButtonClass
-          "
-          @click="verifyEmail"
-        >
+        <button type="submit" :class="'text-2xl p-2 w-1/2 bg-slate-500 self-center ' + submitButtonClass" @click="verifyEmail">
           Send
         </button>
       </form>
@@ -92,15 +53,16 @@ export default {
   data() {
     return {
       messageInputLength: 0,
+      messageMaxLength: 250,
       submitted: false,
-      FORM_ENDPOINT: 'https://public.herotofu.com/v1/2004e660-6504-11ed-891b-4f350712a1f0',
+      FORM_ENDPOINT: process.env.FORM_ENDPOINT,
       isEmailValid: 0,
       forbiddenWords: false,
     };
   },
   computed: {
     getMessageColor() {
-      return this.messageInputLength > 249 ? 'text-red-500' : 'text-slate-400';
+      return this.messageInputLength > (this.messageMaxLength - 1) ? 'text-red-500' : 'text-slate-400';
     },
     submitButtonClass() {
       return this.isEmailValid ? 'cursor-pointer bg-green-500 rounded-2xl' : 'cursor-not-allowed rounded-2xl';
