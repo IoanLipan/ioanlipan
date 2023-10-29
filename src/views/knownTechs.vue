58fd705a71996cb4f18ec725ebafa030978ec918
Lipan Ioan <lipanioan420@gmail.com>
2023-10-29 23:29:08 +0200

    move tech stacks out of view

--- src/views/knownTechs.vue
+++ src/views/knownTechs.vue
@@ -4,13 +4,14 @@
       These are the technologies that I am proficient in:
     </div>
     <div class="col-start-2 col-span-10 flex justify-evenly items-center flex-wrap">
-      <KnownTechItem v-for="tech in technologies" :key="tech.title" :imageName="tech.imageName" :title="tech.title"
+      <KnownTechItem v-for="tech in TECH_STACKS" :key="tech.title" :imageName="tech.imageName" :title="tech.title"
         :description="tech.description" :level="tech.level" />
     </div>
   </div>
 </template>
 
 <script>
+import TECH_STACKS from '../components/techStacks';
 import KnownTechItem from '../components/knownTechItem.vue';
 
 export default {
@@ -19,62 +20,7 @@ export default {
   components: { KnownTechItem },
   data() {
     return {
-      technologies: [
-        {
-          imageName: 'html',
-          title: 'HTML',
-          description: 'HTML (Hypertext Markup Language) is the standard language used for creating the structure and content of web pages.',
-          level: 5,
-        },
-        {
-          imageName: 'css',
-          title: 'CSS',
-          description: 'CSS (Cascading Style Sheets) is a stylesheet language that controls the presentation and appearance of HTML elements on a web page.',
-          level: 5,
-        },
-        {
-          imageName: 'tailwind',
-          title: 'Tailwind',
-          description: 'Tailwind CSS is a utility-first CSS framework that provides a set of pre-defined classes to rapidly build user interfaces.',
-          level: 5,
-        },
-        {
-          imageName: 'javascript',
-          title: 'JavaScript',
-          description: 'JavaScript is a versatile programming language used for adding interactivity and dynamic functionality to websites.',
-          level: 4.5,
-        },
-        {
-          imageName: 'react',
-          title: 'ReactJS',
-          description: 'React is a popular JavaScript library for building user interfaces, offering a component-based approach for creating reusable UI elements.',
-          level: 4.5,
-        },
-        {
-          imageName: 'vue',
-          title: 'VueJS',
-          description: 'Vue.js is a progressive JavaScript framework for building user interfaces, known for its simplicity, flexibility, and performance.',
-          level: 4.5,
-        },
-        {
-          imageName: 'node',
-          title: 'Node.js',
-          description: 'Node.js is a back-end JavaScript runtime environment that runs on the V8 engine and executes JavaScript code outside a web browser.',
-          level: 3,
-        },
-        {
-          imageName: 'mongodb',
-          title: 'MongoDB',
-          description: 'MongoDB is a source-available cross-platform document-oriented database program, classified as a NoSQL database program.',
-          level: 3,
-        },
-        {
-          imageName: 'shopify',
-          title: 'Shopify',
-          description: 'Shopify is a popular e-commerce platform for creating and managing online stores.',
-          level: 4,
-        },
-      ],
+      TECH_STACKS,
     };
   },
 };
