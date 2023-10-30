3a0b012e65c4c2ef59395d8b19175fe2c91c8bc0
Lipan Ioan <lipanioan420@gmail.com>
2023-10-30 13:31:08 +0200

    more project card details (tracking)

--- src/components/projectCard.vue
+++ src/components/projectCard.vue
@@ -13,12 +13,12 @@
       <SvgIcon :name="imageName"
         class="self-center fill-accent h-[180px] md:h-[200px] flex justify-center pt-5 scale-75 md:scale-100" />
       <h3 class="text-2xl text-accent underline pt-2">{{ title }}</h3>
-      <p class="bg-secondary rounded-b-2xl text-textcolor">Click to flip the card!</p>
+      <p class="bg-secondary rounded-b-2xl text-textcolor tracking-widest">Click to flip the card!</p>
     </div>
     <div class="back-of-card bg-neutral rounded-3xl border-4 border-primary cursor-pointer
     flex flex-col justify-between" :style="{ transform: backTransform }">
-      <a v-if="url" :href="url" class="card-border rounded-t-2xl">Click for repo/project!</a>
-      <p v-else class="card-border rounded-t-2xl">The repo/project is confidential</p>
+      <a v-if="url" :href="url" class="card-border rounded-t-2xl tracking-widest">Click for repo/project!</a>
+      <p v-else class="card-border rounded-t-2xl tracking-widest">The repo/project is confidential</p>
       <p class="p-2 xl:px-8 md:text-xl min-h-28 text-textcolor">{{ description }}</p>
       <div class="bg-primary text-xl rounded-b-2xl tracking-widest text-secondary
        w-full mx-auto px-5 py-2 h-20 flex items-center justify-center">
