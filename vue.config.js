6e03072dcf02523c3cfd023041a2a2590f7ea0e1
Lipan Ioan <lipanioan420@gmail.com>
2023-11-13 14:23:10 +0200

    add gzip

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,23 @@
 const { defineConfig } = require('@vue/cli-service');
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins.push(
+        new CompressionWebpackPlugin({
+          filename: '[path].gz[query]',
+          algorithm: 'gzip',
+          test: /\.(js|css|json|txt|html|ico|svg|png|jpg|jpeg)(\?.*)?$/i,
+          threshold: 10240,
+          minRatio: 0.8,
+          deleteOriginalAssets: false,
+        }),
+      );
+    }
+  },
 });
