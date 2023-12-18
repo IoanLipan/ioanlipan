8c32569471fed6ef92e14f2cd40ec045ac0d4423
Lipan Ioan <lipanioan420@gmail.com>
2023-12-18 19:08:26 +0200

    add project images

--- src/components/projectCard.vue
+++ src/components/projectCard.vue
@@ -1,27 +1,48 @@
 <template>
-  <div class="card rounded-3xl
-    items-center text-center w-[320px] sm:w-[45%] h-64 md:h-80 mb-10"
-    :class="{ 'flipped': isFlipped, 'hovered': isHovered }" @click="flipCard" @mouseenter="hoverCard">
-    <div class="front-of-card bg-primary rounded-3xl border-4 border-secondary cursor-pointer
-    flex flex-col justify-between" :style="{ transform: frontTransform }">
-      <div v-if="workRelated" class="bg-accent text-secondary card-tag">
+  <div
+    class="card rounded-3xl items-center text-center w-[320px] sm:w-[45%] h-64 md:h-80 mb-10"
+    :class="{ flipped: isFlipped, hovered: isHovered }"
+    @click="flipCard"
+    @mouseenter="hoverCard"
+  >
+    <div
+      class="front-of-card bg-primary rounded-3xl border-4 border-secondary cursor-pointer flex flex-col justify-between"
+      :style="{ transform: frontTransform }"
+    >
+      <div v-if="workRelated === true" class="bg-accent text-secondary card-tag">
         Work Related
       </div>
       <div v-else class="bg-secondary text-accent card-tag">
         Personal Project
       </div>
-      <SvgIcon :name="imageName"
-        class="self-center fill-accent h-[180px] md:h-[200px] flex justify-center pt-5 scale-75 md:scale-100" />
-      <h3 class="text-2xl text-accent underline pt-2">{{ title }}</h3>
-      <p class="bg-secondary rounded-b-2xl text-textcolor tracking-widest">Click to flip the card!</p>
+      <img
+        :src="imageURL"
+        class="self-center h-[180px] md:h-[200px] rounded-2xl"
+      />
+      <h3 class="text-2xl text-accent underline">{{ title }}</h3>
+      <p class="bg-secondary rounded-b-2xl text-textcolor tracking-widest">
+        Click to flip the card!
+      </p>
     </div>
-    <div class="back-of-card bg-neutral rounded-3xl border-4 border-primary cursor-pointer
-    flex flex-col justify-between" :style="{ transform: backTransform }">
-      <a v-if="url" :href="url" class="card-border rounded-t-2xl tracking-widest">Click for repo/project!</a>
-      <p v-else class="card-border rounded-t-2xl tracking-widest">The repo/project is confidential</p>
-      <p class="p-2 xl:px-8 md:text-xl min-h-28 text-textcolor">{{ description }}</p>
-      <div class="bg-primary text-xl rounded-b-2xl tracking-widest text-secondary
-       w-full mx-auto px-5 py-2 h-20 flex items-center justify-center">
+    <div
+      class="back-of-card bg-neutral rounded-3xl border-4 border-primary cursor-pointer flex flex-col justify-between"
+      :style="{ transform: backTransform }"
+    >
+      <a
+        v-if="url"
+        :href="url"
+        class="card-border rounded-t-2xl tracking-widest"
+        >Click for repo/project!</a
+      >
+      <p v-else class="card-border rounded-t-2xl tracking-widest">
+        The repo/project is confidential
+      </p>
+      <p class="p-2 xl:px-8 md:text-xl min-h-28 text-textcolor">
+        {{ description }}
+      </p>
+      <div
+        class="bg-primary text-xl rounded-b-2xl tracking-widest text-secondary w-full mx-auto px-5 py-2 h-20 flex items-center justify-center"
+      >
         {{ usedTech }}
       </div>
     </div>
@@ -29,7 +50,6 @@
 </template>
 
 <script>
-import SvgIcon from './svgIcon.vue';
 import '../assets/styles/projectCard.css';
 
 export default {
@@ -51,6 +71,10 @@ export default {
       type: String,
       default: '',
     },
+    imageURL: {
+      type: String,
+      default: null,
+    },
     url: {
       type: String,
       default: '',
@@ -60,7 +84,6 @@ export default {
       default: false,
     },
   },
-  components: { SvgIcon },
   data() {
     return {
       isFlipped: false,
--- src/views/webDeveloperPage.vue
+++ src/views/webDeveloperPage.vue
@@ -1,68 +1,81 @@
 <template>
   <div class="grid-of-12-container w-[100vw] mx-auto text-textcolor">
-    <article class="md:text-2xl text-center tracking-widest leading-relaxed
-      col-start-2 col-span-10 md:col-start-3 md:col-span-8 py-10" v-motion-fade>
+    <article
+      class="md:text-2xl text-center tracking-widest leading-relaxed col-start-2 col-span-10 md:col-start-3 md:col-span-8 py-10"
+      v-motion-fade
+    >
       Remember, anything is possible with the right approach!
     </article>
-    <div class="col-start-1 col-span-12 md:col-start-2 md:col-span-10 flex flex-col items-center">
-      <RouterLink v-motion-pop :delay="600" to="/tech-knowledge" class="flex p-3 gap-2 mx-3 rounded-3xl text-center
-          cursor-pointer bg-secondary hover:bg-opacity-90 group text-accent">
-        <SvgIcon name="click" class="rotate-90 group-hover:rotate-[450deg] point-finger" />
+    <div
+      class="col-start-1 col-span-12 md:col-start-2 md:col-span-10 flex flex-col items-center"
+    >
+      <RouterLink
+        v-motion-pop
+        :delay="600"
+        to="/tech-knowledge"
+        class="flex p-3 gap-2 mx-3 rounded-3xl text-center cursor-pointer bg-secondary hover:bg-opacity-90 group text-accent"
+      >
+        <SvgIcon
+          name="click"
+          class="rotate-90 group-hover:rotate-[450deg] point-finger"
+        />
         <div class="min-w-[165px] text-accent point-finger">
           Click here
           <br />
           to see my stack
         </div>
-        <SvgIcon name="click" class="-rotate-90 scale-x-[-1] group-hover:rotate-[270deg] point-finger" />
+        <SvgIcon
+          name="click"
+          class="-rotate-90 scale-x-[-1] group-hover:rotate-[270deg] point-finger"
+        />
       </RouterLink>
-      <p v-motion-pop :delay="700" class="text-center py-4 md:py-8">
-        OR
-      </p>
-      <h3 v-motion-pop :delay="800" class="text-center px-8 md:text-2xl pb-5 md:pb-10 flex justify-between items-center">
+      <p v-motion-pop :delay="700" class="text-center py-4 md:py-8">OR</p>
+      <h3
+        v-motion-pop
+        :delay="800"
+        class="text-center px-8 md:text-2xl pb-5 md:pb-10 flex justify-between items-center"
+      >
         <SvgIcon name="chevron-down" class="stroke-textcolor" />
-        <div>Check out the <span
-            class="text-secondary font-semibold tracking-wider capitalize">&nbsp;projects&nbsp;</span> I worked on below:
+        <div>
+          Check out the
+          <span class="text-secondary font-semibold tracking-wider capitalize"
+            >&nbsp;projects&nbsp;</span
+          >
+          I worked on below:
         </div>
         <SvgIcon name="chevron-down" class="stroke-textcolor" />
       </h3>
-      <article v-motion-pop :delay="900" class="text-normal p-5 lg:px-10 flex flex-row flex-wrap justify-evenly w-full">
-        <ProjectCard title="Pizza Menu" usedTech="ReactJS, CSS"
-          description="A simple pizza menu. You can see if the restaurant is open right now!" imageName="pizza"
-          url="https://pizza-menu-sand.vercel.app/" v-motion-roll-top />
-        <ProjectCard title="Advice App" usedTech="ReactJS"
-          description="Advice app connected to AdvicesApi. Click to see another advice!" imageName="advice"
-          url="https://react-advice-app-topaz.vercel.app/" v-motion-roll-top />
-        <ProjectCard title="What to wear" usedTech="ReactJS, TailwindCSS, genez.io"
-          description="A weather app with suggestions for dressing properly for the weather" imageName="weather"
-          url="https://rose-junior-opossum.app.genez.io/" v-motion-roll-top />
-        <ProjectCard title="UI library" usedTech="VueJS"
-          description="A library with homogenized UI components for developers" imageName="library" workRelated
-          v-motion-roll-top />
-        <ProjectCard title="Guardian" usedTech="Shopify, CSS" url="https://guardian-armour-up.myshopify.com/"
-          description="A e-commerce website for a phone protection company" imageName="protection" v-motion-roll-top />
-        <ProjectCard title="TLC" usedTech="ReactJS, TailWindCSS"
-          description="A e-commerce website for a dentist accessories company" imageName="e-commerce" workRelated
-          url="https://theloupescompany.com/" v-motion-roll-bottom />
-        <div v-if="!showMore" class="secondary-button" @click="ShowMoreProjects()">
+      <article
+        v-motion-pop
+        :delay="900"
+        class="text-normal p-5 lg:px-10 flex flex-row flex-wrap justify-evenly w-full"
+      >
+        <!-- Projects displayed using v-for -->
+        <ProjectCard
+          v-for="(project, index) in displayedProjects"
+          :key="index"
+          :title="project.title"
+          :hasImage="project.hasImage"
+          :usedTech="project.usedTech"
+          :workRelated="project.workRelated"
+          :description="project.description"
+          :imageURL="project.imageURL"
+          :url="project.url"
+          v-motion-roll-top
+        />
+
+        <!-- Toggle button for showing more projects -->
+        <div v-if="!showMore" class="secondary-button" @click="showMore = true">
           ✨ Show More Programming Projects ...
         </div>
-        <ProjectCard v-if="showMore" title="AVM Assistant" usedTech="ReactJS, DJango"
-          description="A web-app to help Auditive, Visualy or Motricly impaired people" imageName="wheelchair"
-          url="https://github.com/IoanLipan/avm_assistant" v-motion-roll-top />
-        <ProjectCard v-if="showMore" title="Custom Video Browser" usedTech="VueJS, YouTubeAPI"
-          description="A custom video browser connected to the YouTubeAPI" imageName="video"
-          url="https://github.com/IoanLipan/Video-browser" v-motion-roll-top />
-        <ProjectCard v-if="showMore" title="Traffic Sign Detector" usedTech="YOLOv4"
-          description="A trained model that detects traffic signs with AI" imageName="sign"
-          url="https://github.com/IoanLipan/Traffic-sign-detection" v-motion-roll-top />
-        <ProjectCard v-if="showMore" title="TicTacToe Game" usedTech="Java"
-          description="TicTacToe that can be played in the console" imageName="game"
-          url="https://github.com/IoanLipan/TicTacToe" v-motion-roll-bottom />
-        <ProjectCard v-if="showMore" title="BattlePlanes Game" usedTech="Python"
-          description="A game of Battleplanes that can be played in the console" imageName="plane"
-          url="https://github.com/IoanLipan/TicTacToe" v-motion-roll-bottom />
       </article>
-      <div v-if="showMore" class="secondary-button w-[240px]" @click="ShowLessProjects()">
+
+      <!-- Toggle button for showing less projects -->
+      <div
+        v-if="showMore"
+        class="secondary-button w-[240px]"
+        @click="showMore = false"
+      >
         ⏫ Show less ...
       </div>
     </div>
@@ -83,15 +96,143 @@ export default {
   data() {
     return {
       showMore: false,
+      projects: [
+        {
+          title: 'Eat-n-Split',
+          usedTech: 'ReactJS',
+          description:
+            'Calculate the check at a shared table!',
+          // eslint-disable-next-line global-require
+          imageURL: require('../assets/images/project_images/eat_n_split.webp'),
+          url: 'https://eat-n-split-amber-mu.vercel.app/',
+        },
+        {
+          title: 'Employee CRM',
+          usedTech: 'ReactJS, MongoDB, ExpressJS, NodeJS, Material-UI',
+          description:
+            'You can create, delete, rename users and add hours to their work planner!',
+          // eslint-disable-next-line global-require
+          imageURL: require('../assets/images/project_images/crm_app.webp'),
+          url: '#',
+        },
+        {
+          title: 'Travel Packing List',
+          usedTech: 'ReactJS',
+          description:
+            'You can create a list for packing and tick away the items!',
+          // eslint-disable-next-line global-require
+          imageURL: require('../assets/images/project_images/travelList.webp'),
+          url: 'https://travel-list-mocha-six.vercel.app/',
+        },
+        {
+          title: 'Reading List',
+          usedTech: 'NextJS, ReactJS, TailwindCSS, Google BooksAPI',
+          description:
+            'You can add books to your reading list and see statistics about reading them!',
+          // eslint-disable-next-line global-require
+          imageURL: require('../assets/images/project_images/reading_list.webp'),
+          url: 'https://reading-list-rho.vercel.app/',
+        },
+        {
+          title: 'Catch The Squirrels!',
+          usedTech: 'React Native',
+          description:
+            'A simple game with a twist. You have to catch all the squirrels!',
+          // eslint-disable-next-line global-require
+          imageURL: require('../assets/images/project_images/catch_the_squirrels.webp'),
+          url: 'https://github.com/IoanLipan/CatchTheSquirrels',
+        },
+        {
+          title: 'Pizza Menu',
+          usedTech: 'ReactJS, CSS',
+          description:
+            'A simple pizza menu. You can see if the restaurant is open right now!',
+          // eslint-disable-next-line global-require
+          imageURL: require('../assets/images/project_images/pizzaMenu.webp'),
+          url: 'https://pizza-menu-sand.vercel.app/',
+        },
+        {
+          title: 'Advice App',
+          usedTech: 'ReactJS',
+          description:
+            'Advice app connected to AdvicesApi. Click to see another advice!',
+          // eslint-disable-next-line global-require
+          imageURL: require('../assets/images/project_images/adviceApp.webp'),
+          url: 'https://react-advice-app-topaz.vercel.app/',
+        },
+        {
+          title: 'What to wear',
+          usedTech: 'ReactJS, TailwindCSS, genez.io',
+          description:
+            'A weather app with suggestions for dressing properly for the weather',
+          // eslint-disable-next-line global-require
+          imageURL: require('../assets/images/project_images/weatherApp.webp'),
+          url: 'https://rose-junior-opossum.app.genez.io/',
+        },
+        {
+          title: 'UI library',
+          usedTech: 'VueJS',
+          description:
+            'A library with homogenized UI components for developers',
+          // eslint-disable-next-line global-require
+          imageURL: require('../assets/images/project_images/ui_library.webp'),
+          url: '#',
+          workRelated: true,
+        },
+        {
+          title: 'TLC',
+          usedTech: 'ReactJS, TailWindCSS',
+          description: 'A e-commerce website for a dentist accessories company',
+          // eslint-disable-next-line global-require
+          imageURL: require('../assets/images/project_images/tlcCommerce.webp'),
+          url: 'https://theloupescompany.com/',
+          workRelated: true,
+        },
+        {
+          title: 'AVM Assistant',
+          usedTech: 'ReactJS, DJango',
+          description:
+            'A web-app to help Auditive, Visualy or Motricly impaired people',
+          // eslint-disable-next-line global-require
+          imageURL: require('../assets/images/project_images/avm_assistant.webp'),
+          url: 'https://github.com/IoanLipan/avm_assistant',
+        },
+        {
+          title: 'Traffic Sign Detector',
+          usedTech: 'YOLOv4',
+          description: 'A trained model that detects traffic signs with AI',
+          // eslint-disable-next-line global-require
+          imageURL: require('../assets/images/project_images/traffic_sign_detection.webp'),
+          url: 'https://github.com/IoanLipan/Traffic-sign-detection',
+        },
+        // {
+        //   title: 'TicTacToe Game',
+        //   usedTech: 'Java',
+        //   description: 'TicTacToe that can be played in the console',
+        //   imageURL: 'game',
+        //   url: 'https://github.com/IoanLipan/TicTacToe',
+        // },
+        // {
+        //   title: 'BattlePlanes Game',
+        //   usedTech: 'Python',
+        //   description:
+        //     'A game of Battleplanes that can be played in the console',
+        //   imageURL: 'plane',
+        //   url: 'https://github.com/IoanLipan/BattlePlanes',
+        // },
+      ],
     };
   },
-  methods: {
-    ShowMoreProjects() {
-      this.showMore = true;
-    },
-    ShowLessProjects() {
-      this.showMore = false;
-      window.scroll(0, 0);
+  computed: {
+    // ShowMoreProjects() {
+    //   this.showMore = true;
+    // },
+    // ShowLessProjects() {
+    //   this.showMore = false;
+    //   window.scroll(0, 0);
+    // },
+    displayedProjects() {
+      return this.showMore ? this.projects : this.projects.slice(0, 4);
     },
   },
 };
